c19354c94ba6b464a6169ef92277306bb3baadd4
vicky <vrossi@itba.edu.ar>
2020-12-02 18:48:20 -0300

    Cambios en el CSV, main y todoList

--- CSV.c
+++ CSV.c
@@ -1,2 +1,34 @@
 #include <stdio.h>
+#include "cityADT.h"
+#include "CSV.h"
+#define BUFFER_SIZE 1024
+#define BLOCK 20
 
+void readCity(FILE * file, cityADT myCity){
+	FILE * myFile;
+ 	myFile= fopen(file, "r");
+
+	if(myFile==NULL){
+    	printf("No abrio correctamente");
+		return NULL;
+	}
+
+char myLine[BUFFER_SIZE]; 
+char * token;
+char ** aux;
+
+
+ while(fgets(myLine, BUFFER_SIZE, myFile)!=NULL) {
+    
+ }
+ fclose(myFile);
+}
+static char ** readLine(char * rta, char * line){
+
+
+	 token= strtok( myLine , ";" );
+	 aux = malloc(sizeof(char*));
+     while ( (aux=strtok(NULL, ";" ))!=NULL){
+        printf("%s\n" ,aux);
+     }
+}
--- bueProces.c
+++ bueProces.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 
-int main(int argc, char const *argv[]){
+int main(int argc, const char *argv[]){
     //leer todo con csv.h
 
     //vamos a pasarselo al back con cityadt para las queries
@@ -18,15 +18,8 @@ int main(int argc, char const *argv[]){
         fprintf(stderr,"Invalid amount of arguments"); /* esto nose si esta bien, hay que separar front de back */
         return 1;
     }
-
-   FILE * fp = fopen(argv[1] , "r");
-   FILE * fp2= fopen(argv[2], "r");
-   if(fp == NULL || fp2 == NULL){
-      fprintf(stderr,"Error opening file");
-      return 1;
-   }
-
-    fclose(fp); fclose(fp2);
+    cityADT myCity = newCity();
+    readCity(argv[2],myCity);
     return 0;
 }
 
