b12e49b467d6c4b6771c5e1721ca90da0a4f5de4
vicky <vrossi@itba.edu.ar>
2020-12-03 18:45:00 -0300

    Cambios menores en el .h

--- CSV.h
+++ CSV.h
@@ -3,21 +3,14 @@
 #include <stdio.h>
 
 /*
-** Funcion que lee lineas y se encarga de agregar
-** al ADT los nodos correspondientes a cada barrio
-** con su cantidad total de habitantes
-** retorna 1 si lo agrego correctamente
-** retorna -1 en caso de error al abrir el archivo
-** retorna -2 en caso de error al intentar agregar el barrio
+** Funcion que lee lineas y agrega al ADT los nodos correspondientes a cada barrio con su cantidad total de habitantes.
+** Retorna 1 si lo agrego correctamente, -1 en caso de error al abrir el archivo y -2 en caso de error al intentar agregar el barrio
 */
 int readCity( const char* file, cityADT c);
 
 /*
-** Funcion que lee lineas y se encarga de agregar
-** al ADT los campos necesarios asociados a cada arbol
-** retorna 1 si lo agrego correctamente
-** retorna -1 en caso de error.
-** retorna -2 en caso de error al intentar agregar el arbol
+** Funcion que lee lineas y agrega al ADT los campos necesarios asociados a cada arbol.
+** Retorna 1 si lo agrego correctamente, -1 en caso de errory -2 en caso de error al intentar agregar el arbol.
 */
 int readTree( const char *file, cityADT c);
 
--- cityADT.h
+++ cityADT.h
@@ -14,13 +14,14 @@ cityADT newCity(void);
 /* Libera los recursos utilizados por el TAD */
 void freeCity(cityADT c);
 
-/* devuelve 1 si lo pudo agregar dado que la lista no contiene repetidos
+/* Agrega un barrio. 
+** devuelve 1 si lo pudo agregar dado que la lista no contiene repetidos,
 ** 0 si no pues hay repetidos o ERROR
 */
 int addNeigh(cityADT c, char * neigh, size_t cantHab);
 
 /*
-** Agrega un arbol a un barrio si el barrio es valido. Retorna 1 si pudo agregarlo 0 si no, o ERROR
+** Agrega un arbol a un barrio si el barrio es valido. Retorna 1 si pudo agregarlo 0 si no existe el barrio, o ERROR
 ** Si es un nuevo arbol hace una COPIA del nombre del arbol. Si esta repetido, incrementa la cantidad de ese arbol
 ** en el barrio dado. 
 ** Incrementa la cantidad de arboles totales en el barrio.
@@ -28,22 +29,21 @@ int addNeigh(cityADT c, char * neigh, size_t cantHab);
 int addTree(cityADT c, char * neigh, char *tree);
 
 /*
-** Devuelve un vector de chars con los nombres cientificos de los arboles mas populares de cada barrio
-** dim parametro de entrada y salida con la cantidad de barrios
-** el vector esta ordenado de manera alfabetica por barrio.
-** retorna NULL en caso de ERROR, dim en 0 si esta vacio
+** Devuelve un vector de chars ordenado alfabeticamente,con los nombres cientificos de los arboles mas populares de cada barrio.
+** Dim parametro de entrada y salida con la cantidad de barrios.
+** Retorna NULL en caso de ERROR y dim en 0 si esta vacio.
 */
 char ** mostPopularTree(cityADT c , size_t * dim);
 
 /*
-**devuelve barrios en orden alfabético
-** dim parametro de entrada y salida con la cantidad de barrios
-** retorna NULL en caso de ERROR, dim en 0 si esta vacio
+** Devuelve un vector de chars de los barrios en orden alfabético.
+** Dim parametro de entrada y salida con la cantidad de barrios.
+** Retorna NULL en caso de ERROR y dim en 0 si esta vacio o hubo algo ERROR.
 */
 char ** showAllNeigh( cityADT c , size_t *dim );
 
-/*devuelve cant de arboles por persona en cada barrio (alfabeticamente)
-** retorna NULL y dim en -1 en caso de ERROR, dim en 0 si esta vacio
+/* Devuelve la cantidad de arboles por persona en cada barrio, ordenado alfabeticamente.
+** Retorna NULL y dim en -1 en caso de ERROR o dim en 0 si esta vacio.
 */
 double* treesPerPerson ( cityADT c, char **neighs , size_t *dim);
 
