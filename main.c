da9dcd4c9c39195181371ffea46b2585b3dae6e4
vicky <vrossi@itba.edu.ar>
2020-12-03 19:00:27 -0300

    Cambios en el main

--- main.c
+++ main.c
@@ -34,26 +34,26 @@ int main(int argc, const char *argv[]){
         exit(1);
     }
     char err = 0;
-    FILE * QUERY1= fopen("query1.csv","w");
-    FILE * QUERY2= fopen("query2.csv","w");
+    FILE * QUERY1= fopen("query1.csv","w"); //creacion del archivo con el total de arboles por habitantes por barrio
+    FILE * QUERY2= fopen("query2.csv","w"); //creacion del archivo con la especie de árbol más popular por barrio
 
     size_t q1;
     char **neighs1 = showAllNeigh( myCity , &q1);
     double *query1=treesPerPerson(myCity, neighs1, &q1);
 
     if (  neighs1!= NULL && query1!=NULL ){
-        fprintf(QUERY1,"BARRIO;PROMEDIO_ARBOLES_HABITANTES\n");
+        fprintf(QUERY1,"BARRIO;PROMEDIO_ARBOLES_HABITANTES\n"); //Head del archivo 1
         for (size_t i = 0; i < q1; i++){
-            fprintf(QUERY1,"%s;", neighs1[i]);
-            fprintf(QUERY1,"%g\n", query1[i]);
-            free(neighs1[i]);
+            fprintf(QUERY1,"%s;", neighs1[i]); //Agregamos barrios ya ordenados
+            fprintf(QUERY1,"%g\n", query1[i]); //Agregamos el promedio 
+            free(neighs1[i]); 
         }
     }
     else{
-        fprintf(QUERY1,"ERROR\n");
+        fprintf(QUERY1,"ERROR\n"); //En caso de que neighs y query1 sean NULL, indicaria que o no pudo generar espacio o no hay barrios, entonces en el archivo solamente indicaria ERROR.
         err = 1;
     }
-    fclose(QUERY1);
+    fclose(QUERY1); //Cerramos el archivo y liberamos los recursos utilizados
     free(query1);
     free(neighs1);
 
@@ -62,7 +62,7 @@ int main(int argc, const char *argv[]){
     char **neighs2 =showAllNeigh(myCity , &q2 );
 
     if (  neighs2!= NULL && query2!=NULL ){
-        fprintf(QUERY2,"BARRIO;NOMBRE_CIENTIFICO\n");
+        fprintf(QUERY2,"BARRIO;NOMBRE_CIENTIFICO\n"); //Head del archivo 2
         for (size_t i = 0; i < q2 ; i++){
             fprintf(QUERY2,"%s;", neighs2[i]);
             fprintf(QUERY2,"%s\n", query2[i]);
@@ -71,7 +71,7 @@ int main(int argc, const char *argv[]){
         }
     }
     else{
-        fprintf(QUERY2,"Error\n");
+        fprintf(QUERY2,"ERROR\n"); //En caso de que neighs y query2 sean NULL, indicaria que o no pudo generar espacio o no hay barrios, entonces en el archivo solamente indicaria ERROR.
         err = 1;
     }
     fclose(QUERY2);
