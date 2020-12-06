daf5b878ccecca25d8c9923f2f9856d6f87bef8c
vicky <vrossi@itba.edu.ar>
2020-12-06 10:37:14 -0300

    Explicacion del codigo del main.c

--- main.c
+++ main.c
@@ -18,41 +18,44 @@ Error de heap: 3
 void freeRemaining(char **vec,size_t dim);
 
 int main(int argc, char const *argv[]){
-    if(argc!=3){
+    
+    if(argc!=3){                                                //Chequeo que la cantidad de archivos (3) incresada sea valida 
         fprintf(stderr,"Numero de argumentos invalidos\n"); 
         return 1;
     }
     
-    cityADT myCity = newCity();
+    cityADT myCity = newCity();                                 //Creo una cuidad     
 
-    if(myCity == NULL){
+    if(myCity == NULL){                                         //Si myCity es NULL entonces fallo al asignarle lugar el calloc
         fprintf(stderr, "Error de heap\n");
         return 3;
     }
-    //abro el archivo y lo cargo, así hago un único fclose en caso de error
-    FILE* nFile=open(argv[NEIGH_FILE],"r");
+
+    FILE* nFile=open(argv[NEIGH_FILE],"r");                     //Abro el archivo y lo cargo, así hago un único fclose en caso de error
     if ( nFile==NULL  ){
         fprintf(stderr,"Error al abrir archivo de los barrios\n");
         freeCity(myCity);
         return 2;
     }
-    int error=read( nFile,myCity, CITY);
-    if ( error==-1 ){
+
+    int error=read( nFile,myCity, CITY);                        //Leo le archivo y al mismo tiempo guardo los datos pedidos
+    if ( error==-1 ){                                           //Si hubo algun error, libero los recursos utilizados, cierro el archivo y corto
         freeCity(myCity);
         fclose(nFile);
         fprintf(stderr,"Error al cargar los datos de los barrios (heap)\n");
         return 3;
     }
     
-    FILE* tFile=open(argv[TREE_FILE],"r");
-    if ( tFile==NULL  ){
+    FILE* tFile=open(argv[TREE_FILE],"r");                      //Abro el archivo y lo cargo
+    if ( tFile==NULL  ){                                        //Si hubo algun error cierro el archivo y libero la cuidad creada
         fprintf(stderr,"Error al abrir archivo de los arboles\n");
         fclose(nFile);
         freeCity(myCity);
         return 2;
     }
-    error=read( tFile , myCity, TREE);
-    if ( error==-1 ){
+
+    error=read( tFile , myCity, TREE);                          //Leo el archivo y guardo los datos pedidos
+    if ( error==-1 ){                                           //Si hubo algun error libero la cuidad creada 
         freeCity(myCity);
         //fclose(tFile);
         fprintf(stderr,"Error al cargar los datos de los arboles (heap)\n");
@@ -60,30 +63,32 @@ int main(int argc, char const *argv[]){
     }
     size_t n;
     //Ahora vamos a cargar los vectores que nos van a servir para resolver las querys.
-    char** neighs=NULL; char** query2=NULL;  double* query1=NULL; 
+    char** neighs=NULL; 
+    char** query2=NULL;  
+    double* query1=NULL; 
 
     //revisar mañana agregando nulls en retrievedata
-    if(retrieveData(myCity,&neighs,&query2,&query1,&n)==-1){
-        freeCity(myCity);
+    if(retrieveData(myCity,&neighs,&query2,&query1,&n)==-1){    //Llenamos los vectores de los barrios, los promedios y los arbolesmas populares 
+        freeCity(myCity);                                       //En caso de error libero la cuidad creada
         fprintf(stderr,"Error en el heap\n");
         return 3;
     }
-    //ya se han cargado todos los datos necesarios para generar las queries, puede liberarse el TAD
+    //Como ya se han cargado todos los datos necesarios para generar las queries, puede liberarse el TAD
     freeCity(myCity);     
 
-
-    //creacion del archivo con la especie de árbol más popular por barrio
+    //Creacion del archivo con la especie de árbol más popular por barrio
     FILE * QUERY2=open("query2.csv","w"); 
-    if ( QUERY2==NULL  ){
+    if ( QUERY2==NULL  ){                                           
         fprintf(stderr,"Error al crear el archivo de query2\n");
-        freeRemaining(neighs , n);
+        freeRemaining(neighs , n);                                  //Si fallo al abrirse libero los recursos ya ultizados por los vectoes de barrios y arboles mas populares
         freeRemaining(query2 , n);
-        free(query1);
+        free(query1);                                               //y libero el vector de los promedios
         return 2;
     }
-    genQ2( QUERY2 , neighs , query2 , n);
 
-    /* creo que ya esta chequeado en el mismo dataType, pero revisar por las dudas
+    genQ2( QUERY2 , neighs , query2 , n);                           //Lleno el archivo query2.csv con los datos de los barrios y los arboles mas populares
+
+    /* creo que ya esta chequeado en el mismo dataType, pero revisar por las dudas. SIP, cuando agregamos el barrio o el arbol preguntamos por error y cortamos. Borro esto?
     if ( query1==NULL ){
         freeRemaining(neighs , n);
         freeRemaining(query2 , n);
@@ -95,11 +100,11 @@ int main(int argc, char const *argv[]){
     FILE * QUERY1=open("query1.csv","w");
     if ( QUERY1==NULL  ){
         fprintf(stderr,"Error al crear el archivo de query1\n");
-        freeRemaining(neighs , n);
-        free(query1);
+        freeRemaining(neighs , n);                                  //Si fallo al abrirse libero los recursos ya ultizados por el vector de barrios
+        free(query1);                                               //Libero el vector de promedios. No hace falta liberar el vector de arboles mas populares porque genQ2 ya libera.
         return 2;
     }
-    genQ1(QUERY1 , neighs , query1, n);
+    genQ1(QUERY1 , neighs , query1, n);                             //Guardamos en el query1.csv la informacion pedida
 
     return 0;
 }
